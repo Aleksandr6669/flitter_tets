934c1e9e5bf511f3621d75eee27e528066eb7516
gabreil666 <gabreil666@gmail.com>
2025-12-03 11:13:28 +0000

    add _openDropdown

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Beautiful Auth',
+      title: 'NextLevel',
       theme: ThemeData.dark(),
       locale: _locale,
       localizationsDelegates: [
