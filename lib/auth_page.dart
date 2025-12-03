a7a94c05bd51cb6fb48fe13717a5817314aa0355
gabreil666 <gabreil666@gmail.com>
2025-12-03 11:10:12 +0000

    add _openDropdown

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -118,19 +118,17 @@ class _AuthPageState extends State<AuthPage> {
           ),
           Center(
             child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: GlassmorphicAuthForm(
-                  formKey: _formKey,
-                  emailController: _emailController,
-                  passwordController: _passwordController,
-                  confirmPasswordController: _confirmPasswordController,
-                  changeLanguage: widget.changeLanguage,
-                  isLogin: _isLogin,
-                  isLoading: _isLoading,
-                  onToggleFormType: _toggleFormType,
-                  handleAuthAction: _handleAuthAction,
-                ),
+              padding: const EdgeInsets.all(24.0),
+              child: GlassmorphicAuthForm(
+                formKey: _formKey,
+                emailController: _emailController,
+                passwordController: _passwordController,
+                confirmPasswordController: _confirmPasswordController,
+                changeLanguage: widget.changeLanguage,
+                isLogin: _isLogin,
+                isLoading: _isLoading,
+                onToggleFormType: _toggleFormType,
+                handleAuthAction: _handleAuthAction,
               ),
             ),
           ),
@@ -140,7 +138,7 @@ class _AuthPageState extends State<AuthPage> {
   }
 }
 
-class GlassmorphicAuthForm extends StatelessWidget {
+class GlassmorphicAuthForm extends StatefulWidget {
   const GlassmorphicAuthForm({
     super.key,
     required this.formKey,
@@ -165,6 +163,11 @@ class GlassmorphicAuthForm extends StatelessWidget {
   final void Function(BuildContext) handleAuthAction;
 
   @override
+  State<GlassmorphicAuthForm> createState() => _GlassmorphicAuthFormState();
+}
+
+class _GlassmorphicAuthFormState extends State<GlassmorphicAuthForm> {
+  @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
     final locale = Localizations.localeOf(context);
@@ -173,173 +176,184 @@ class GlassmorphicAuthForm extends StatelessWidget {
       orElse: () => supportedLanguages.first,
     );
 
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxWidth: 400,
-        maxHeight: 720, // Fixed height
-      ),
-      child: GlassmorphicContainer(
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: MediaQuery.of(context).size.height * 0.8, // Fixed height
-        borderRadius: 20,
-        blur: 10,
-        alignment: Alignment.center,
-        border: 0,
-        linearGradient: kGlassmorphicGradient,
-        borderGradient: kGlassmorphicBorderGradient,
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  child: Text(
-                    isLogin ? l10n.login : l10n.createAccount,
-                    key: ValueKey(isLogin),
-                    textAlign: TextAlign.center,
-                    style: kTitleTextStyle,
-                  ),
+    final double loginHeight = 550;
+    final double registrationHeight = 640;
+    final targetHeight = widget.isLogin ? loginHeight : registrationHeight;
+
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(end: targetHeight),
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+      builder: (context, height, child) {
+        return ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 400,
+          ),
+          child: GlassmorphicContainer(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: height,
+            borderRadius: 20,
+            blur: 10,
+            alignment: Alignment.center,
+            border: 0,
+            linearGradient: kGlassmorphicGradient,
+            borderGradient: kGlassmorphicBorderGradient,
+            child: child,
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Form(
+          key: widget.formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: Text(
+                  widget.isLogin ? l10n.login : l10n.createAccount,
+                  key: ValueKey(widget.isLogin),
+                  textAlign: TextAlign.center,
+                  style: kTitleTextStyle,
                 ),
-                const SizedBox(height: 10),
-                AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  child: Text(
-                    isLogin
-                        ? l10n.welcomeBack
-                        : l10n.joinUsToStartYourJourney,
-                    key: ValueKey(isLogin),
-                    textAlign: TextAlign.center,
-                    style: kSubtitleTextStyle,
-                  ),
+              ),
+              const SizedBox(height: 10),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: Text(
+                  widget.isLogin
+                      ? l10n.welcomeBack
+                      : l10n.joinUsToStartYourJourney,
+                  key: ValueKey(widget.isLogin),
+                  textAlign: TextAlign.center,
+                  style: kSubtitleTextStyle,
                 ),
-                const SizedBox(height: 30),
-                TextFormField(
-                  controller: emailController,
-                  style: const TextStyle(color: Colors.white),
-                  decoration:
-                      kInputDecoration(l10n.email, Icons.email_outlined),
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        !value.contains('@')) {
-                      return l10n.pleaseEnterYourEmail;
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  style: const TextStyle(color: Colors.white),
-                  decoration:
-                      kInputDecoration(l10n.password, Icons.lock_outline),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return l10n.pleaseEnterYourPassword;
-                    }
-                    return null;
-                  },
-                ),
-                AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  transitionBuilder: (child, animation) {
-                    return SizeTransition(
-                      sizeFactor: animation,
-                      child: child,
-                    );
-                  },
-                  child: !isLogin
-                      ? Column(
-                          children: [
-                            const SizedBox(height: 20),
-                            TextFormField(
-                              controller: confirmPasswordController,
-                              obscureText: true,
-                              style: const TextStyle(color: Colors.white),
-                              decoration: kInputDecoration(
-                                  l10n.confirmPassword, Icons.lock_reset),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return l10n.pleaseConfirmYourPassword;
-                                }
-                                if (value != passwordController.text) {
-                                  return l10n.passwordsDoNotMatch;
-                                }
-                                return null;
-                              },
-                            ),
-                          ],
-                        )
-                      : const SizedBox.shrink(),
-                ),
-                const SizedBox(height: 20),
-                LanguageSelector(
-                  selectedLanguage: selectedLanguage,
-                  onLanguageChange: (Language? newLanguage) {
-                    if (newLanguage != null) {
-                      changeLanguage(Locale(newLanguage.code));
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  decoration: kButtonBoxDecoration,
-                  child: ElevatedButton(
-                    onPressed: isLoading ? null : () => handleAuthAction(context),
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 18),
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                    ),
-                    child: isLoading
-                        ? const CircularProgressIndicator(color: Colors.white)
-                        : AnimatedSwitcher(
-                            duration: const Duration(milliseconds: 300),
-                            child: Text(
-                              isLogin ? l10n.login : l10n.signUp,
-                              key: ValueKey(isLogin),
-                              style: kButtonTextStyle,
-                            ),
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                controller: widget.emailController,
+                style: const TextStyle(color: Colors.white),
+                decoration:
+                    kInputDecoration(l10n.email, Icons.email_outlined),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      !value.contains('@')) {
+                    return l10n.pleaseEnterYourEmail;
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: widget.passwordController,
+                obscureText: true,
+                style: const TextStyle(color: Colors.white),
+                decoration:
+                    kInputDecoration(l10n.password, Icons.lock_outline),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return l10n.pleaseEnterYourPassword;
+                  }
+                  return null;
+                },
+              ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return SizeTransition(
+                    sizeFactor: animation,
+                    child: child,
+                  );
+                },
+                child: !widget.isLogin
+                    ? Column(
+                        children: [
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            controller: widget.confirmPasswordController,
+                            obscureText: true,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: kInputDecoration(
+                                l10n.confirmPassword, Icons.lock_reset),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return l10n.pleaseConfirmYourPassword;
+                              }
+                              if (value != widget.passwordController.text) {
+                                return l10n.passwordsDoNotMatch;
+                              }
+                              return null;
+                            },
                           ),
+                        ],
+                      )
+                    : const SizedBox.shrink(),
+              ),
+              const SizedBox(height: 20),
+              LanguageSelector(
+                selectedLanguage: selectedLanguage,
+                onLanguageChange: (Language? newLanguage) {
+                  if (newLanguage != null) {
+                    widget.changeLanguage(Locale(newLanguage.code));
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              Container(
+                decoration: kButtonBoxDecoration,
+                child: ElevatedButton(
+                  onPressed: widget.isLoading ? null : () => widget.handleAuthAction(context),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
                   ),
+                  child: widget.isLoading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 300),
+                          child: Text(
+                            widget.isLogin ? l10n.login : l10n.signUp,
+                            key: ValueKey(widget.isLogin),
+                            style: kButtonTextStyle,
+                          ),
+                        ),
                 ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AnimatedSwitcher(
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: Text(
+                      widget.isLogin
+                          ? l10n.dontHaveAnAccount
+                          : l10n.alreadyHaveAnAccount,
+                      key: ValueKey(widget.isLogin),
+                      style: const TextStyle(color: Colors.white70),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: widget.onToggleFormType,
+                    child: AnimatedSwitcher(
                       duration: const Duration(milliseconds: 300),
                       child: Text(
-                        isLogin
-                            ? l10n.dontHaveAnAccount
-                            : l10n.alreadyHaveAnAccount,
-                        key: ValueKey(isLogin),
-                        style: const TextStyle(color: Colors.white70),
+                        widget.isLogin ? ' ${l10n.signUp}' : ' ${l10n.login}',
+                        key: ValueKey(widget.isLogin),
+                        style: kTextLinkStyle,
                       ),
                     ),
-                    GestureDetector(
-                      onTap: onToggleFormType,
-                      child: AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 300),
-                        child: Text(
-                          isLogin ? ' ${l10n.signUp}' : ' ${l10n.login}',
-                          key: ValueKey(isLogin),
-                          style: kTextLinkStyle,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
